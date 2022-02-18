0ec48439aff26705c0bc96a344712773bee2d20d
Alex <cupokoffi@gmail.com>
2022-02-18 08:15:49 -0500

    ll

---
+++ Stevens_Spring_2022/CPE-462-A/image.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+ // Read the image file
+ Mat image = imread("/Alex/Users/Alex/Downloads/plank.jpg");
+
+ // Check for failure
+ if (image.empty()) 
+ {
+  cout << "Could not open or find the image" << endl;
+  cin.get(); //wait for any key press
+  return -1;
+ }
+
+ String windowName = "The Guitar"; //Name of the window
+
+ namedWindow(windowName); // Create a window
+
+ imshow(windowName, image); // Show our image inside the created window.
+
+ waitKey(0); // Wait for any keystroke in the window
+
+ destroyWindow(windowName); //destroy the created window
+
+ return 0;
+}
