093e9131690f7e2d378fcd6a70692c8405004777
Alex Gaskins <cupokoffi@gmail.com>
2023-01-30 14:58:59 -0500

    CPE551 HW0 init

---
+++ Courses/Stevens_Spring_2023/CPE-551/Homework/HW0/answer.py
@@ -0,0 +1,11 @@
+
+'''
+Write following code below the comment:
+
+return "I am really excited to learn Python"
+
+'''
+
+def print():
+    # write your code here
+    return "I am really excited to learn Python"
